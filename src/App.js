83b4728f5b12d641e03f8ce112c041531b9abaff
William Ferreira Nunes <willianfn@msn.com>
2020-06-20 01:20:29 -0300

    list, save and delete a repository

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => setProjects(response.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `New Project ${new Date()}`,
+      url: "letslaunch.com.br",
+      techs: ["Nodejs", "ReactJS", "Javascrip"],
+    });
+    setProjects([...projects, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    const index = projects.findIndex((project) => project.id === id);
+    const newArr = [...projects];
+    newArr.splice(index, 1);
+    setProjects(newArr);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {projects.map((project) => (
+          <li key={project.id}>
+            {project.title}
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
